211b31431c57848c960696d32ad5cc1988772eb4
EduardoMichelDev <lalomichel2@gmail.com>
2024-07-30 11:51:04 -0600

    Make localStorageKey and loadFromStorage

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,13 +1,13 @@
 class Cart {
   cartItems;
-  localStorageKey;
+  #localStorageKey;
 
   constructor(localStorageKey) {
-  this.localStorageKey = localStorageKey;
-  this.loadFromStorage();
+  this.#localStorageKey = localStorageKey;
+  this.#loadFromStorage();
   }
 
-  loadFromStorage() {
+  #loadFromStorage() {
     this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
   
   if (!this.cartItems) {
@@ -24,7 +24,7 @@ class Cart {
   }
 
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
   addtoCart(productId) {
     let matchingItem;
@@ -106,6 +106,7 @@ class Cart {
 
 const cart = new Cart('cart-oop');//instance of the class
 const businessCart = new Cart('cart-business');//instance of the class 
+
 
 
 
